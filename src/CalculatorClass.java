cd6b2685f41548b1bf8c296c63e26347841ee52d
Kirai <i.koryciarz@ncl.ac.uk>
2020-10-11 14:59:28 +0100

    First draft of the functionality of the calculator - just ideas for now. Will add real division and percentage as well as user input (probably without GUI first, as a practice)

--- src/CalculatorClass.java
+++ src/CalculatorClass.java
@@ -1,3 +1,34 @@
 public class CalculatorClass {
-    //class for the calculator algorithms
+
+    public static void main(String[] args) {
+        System.out.println(calculations(50, 4, '%'));
+
+    }
+
+    public static long calculations(long n, long m, char operation){
+        long result = 0;
+        switch (operation){
+            case '/':
+                // this won't work properly cuz the result is a long - just pitching ideas
+                double nn = ((double) n);
+                double mm = ((double)m);
+                result = (long) (nn/mm);
+                break;
+            case '*':
+                result = n * m;
+                break;
+            case '+':
+                result = n + m;
+                break;
+            case '-':
+                result = n - m;
+                break;
+            case '%':
+                // same as in the case of division
+                result = (n/100) * m;
+                break;
+        }
+
+        return result;
+    }
 }
